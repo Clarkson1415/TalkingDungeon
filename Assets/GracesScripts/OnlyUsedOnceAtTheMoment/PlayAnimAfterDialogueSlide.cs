59e42c4afe9e5d3d089384ba2774487f6cc5bf07
Clarkson <graceclarkson1415@gmail.com>
2025-04-18 20:57:30 +1000

    torch animated silly working

--- Assets/GracesScripts/OnlyUsedOnceAtTheMoment/PlayAnimAfterDialogueSlide.cs
+++ Assets/GracesScripts/OnlyUsedOnceAtTheMoment/PlayAnimAfterDialogueSlide.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 #nullable enable
 
 public class PlayAnimAfterDialogueSlide : MonoBehaviour
 {
     private GameObject? dialogueBox;
     [SerializeField] private Animator animator;
+    private TorchState state = TorchState.waiting;
+
+    private enum TorchState
+    {
+        waiting, 
+        talking,
+        finishedTalking,
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,29 +22,30 @@ public class PlayAnimAfterDialogueSlide : MonoBehaviour
 
     private void Update()
     {
-        if (dialogueBox != null) // if is not null it is found.
+        switch (this.state)
         {
-            Debug.Log("dialogueBox != null");
-
-            if (!dialogueBox.activeSelf) // if no longer active
-            {
-                Debug.Log("dialogue de activated.");
-                OnDialogueBoxClose();
-                this.dialogueBox = null;
-            }
+            case TorchState.waiting:
+                if (this.dialogueBox == null)
+                {
+                    var diaBox = FindObjectOfType<DialogueTextBox>();
+                    this.dialogueBox = diaBox.gameObject;
+                }
+                else if (this.dialogueBox.activeSelf)
+                {
+                    this.state = TorchState.talking;
+                }
+                break;
+            case TorchState.talking:
+                if (!this.dialogueBox.activeSelf)
+                {
+                    this.state = TorchState.finishedTalking;
+                    OnDialogueBoxClose();
+                }
+                break;
+            case TorchState.finishedTalking:
+                break;
         }
-        else
-        {
-            Debug.Log("dialogueBox == null");
 
-            var diaBox = FindObjectOfType<DialogueTextBox>();
-
-            if (diaBox != null)
-            {
-                Debug.Log("dialogue box found");
-                this.dialogueBox = diaBox.gameObject;
-            }
-        }
     }
 
     private void OnDialogueBoxClose()
