413d69052a419f335ddbb33252695b49119df09c
Clarkson <graceclarkson1415@gmail.com>
2025-05-30 11:15:14 +1000

    added presets

---
+++ Assets/GracesScripts/ScriptableObjects/Valuable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.GracesScripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Valuable", menuName = "DungeonItems/Valuable", order = 1)]
+    public class Valuable : DungeonItem
+    {
+        public override string Path { get => $"Items/Valuable/{this.Name}"; set => throw new System.NotImplementedException(); }
+    }
+}
