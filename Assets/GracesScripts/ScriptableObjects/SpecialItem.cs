fd72f960a813aeec2492d721b3751835886a3ff8
Clarkson <graceclarkson1415@gmail.com>
2025-05-30 15:24:59 +1000

    added blacksmith

--- Assets/GracesScripts/ScriptableObjects/SpecialItem.cs
+++ Assets/GracesScripts/ScriptableObjects/SpecialItem.cs
@@ -8,6 +8,6 @@ namespace Assets.GracesScripts.ScriptableObjects
     public class SpecialItem : DungeonItem
     {
         public override string Path { get => $"Items/SpecialItem/{this.Name}"; set => throw new NotImplementedException("we never need to set this"); }
-        public List<AbilityEffect> Effects { get; set; }
+        public List<AbilityEffect> Effects;
     }
 }
